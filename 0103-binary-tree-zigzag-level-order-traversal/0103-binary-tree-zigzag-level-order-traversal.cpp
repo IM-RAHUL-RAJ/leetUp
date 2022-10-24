82a8d7e1042c8c4598be76d76aee66f2c66cf4c7
Rahul Raj <63979293+IM-RAHUL-RAJ@users.noreply.github.com>
2022-10-24 14:22:36 +0530

    Time: 4 ms (63.97%), Space: 11.9 MB (99.26%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -14,65 +14,38 @@ public:
     
     
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-//         vector<vector<int>>v;
-//         if(root==NULL) return v;
-//         // vector<int>ans;
-//         queue<int>q;
+        vector<vector<int>>v;
+        if(root==NULL) return v;
+        // vector<int>ans;
+        queue<TreeNode*>q;
         
-//         q.push(root->val);
-//         int f=0;
-//         vector<int>temp;
-//         while(!q.empty()){
-            
-//             int sz=q.size();
-//             while(sz--){
-//                 int x=q.front();
-//                 q.pop();
-//                 //temp.push_back(x);
-//                 if(root->left!=NULL) {q.push(root->left->val);}
-//                 if(root->right!=NULL){ q.push(root->right->val);}
-//                 temp.push_back(x);
-                
-//             }
-//             if(f%2!=0) {reverse(temp.begin(),temp.end());}
-        
-//             v.push_back(temp);
-//             temp.clear();
-            
-//             f++;
-//         }
-        
-//         return v;
-       vector<vector<int>> ans1;
-        if(root == NULL)
-            return ans1;
-        
-        queue<TreeNode*> q;
         q.push(root);
-        bool leftToRight = true;
+        int f=0;
         
         while(!q.empty()){
-            int size = q.size();
-            vector<int> ans2(size);
             
-            //level process
-            for(int i = 0 ; i < size ; i++){
-                TreeNode* temp = q.front();
+            int sz=q.size();
+            vector<int>temp;
+            while(sz--){
+                
+                TreeNode* x=q.front();
                 q.pop();
+               
+                if(x->left!=NULL) {q.push(x->left);}
+                if(x->right!=NULL){ q.push(x->right);}
+                temp.push_back(x->val);
                 
-                int index = leftToRight ? i : size - i - 1;
-                ans2[index] = temp->val;
-                
-                if(temp->left)
-                    q.push(temp->left);
-                if(temp->right)
-                    q.push(temp->right);
             }
-            //direction reversal
-            leftToRight = !leftToRight;
-            ans1.push_back(ans2);
+            if(f%2!=0) {reverse(temp.begin(),temp.end());}
+        
+            v.push_back(temp);
+            // temp.clear();
+            
+            f++;
         }
-        return ans1;
+        
+        return v;
+
         
     }
 };
